390f48f25d956c3bf92c60a9efaad0ebe7b6579f
alittlebitofit <42311431+alittlebitofit@users.noreply.github.com>
2019-02-06 21:20:17 +0530

    created 3 functions for individual chords

--- app.js
+++ app.js
@@ -1,27 +1,40 @@
-document.addEventListener("keydown", a);
+document.addEventListener("keydown", playChord);
 
-function a() {
+function playChord() {
   let randomNumber = Math.floor(((Math.random() * 3))) + 1;
 
   switch (randomNumber) {
     case 1:
       console.log("A");
-      let A = new Audio("sounds/A.mp3");
-      A.play();
+      aChord();
       setTimeout("window.alert('A!')", 5000);
       break;
     case 2:
       console.log("D");
-      let D = new Audio("sounds/D.mp3");
-      D.play();
+      dChord();
       setTimeout("window.alert('D!')", 5000);
       break;
     case 3:
       console.log("E");
-      let E = new Audio("sounds/E.mp3");
+      eChord();
       setTimeout("window.alert('E!')", 5000);
-      E.play();
       break;
     default:
   }
+}
+
+function aChord() {
+  let A = new Audio("sounds/A.mp3");
+  A.play();
+}
+
+function dChord() {
+  let D = new Audio("sounds/D.mp3");
+  D.play();
+}
+
+
+function eChord() {
+  let E = new Audio("sounds/E.mp3");
+  E.play();
 }
