edf6b46ca2017c607c51b98f3f6546cd992286e9
alittlebitofit <42311431+alittlebitofit@users.noreply.github.com>
2019-02-06 13:00:32 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5,16 +5,19 @@ function a() {
 
   switch (randomNumber) {
     case 1:
+      console.log("A");
       let A = new Audio("sounds/A.mp3");
       A.play();
       setTimeout("window.alert('A!')", 5000);
       break;
     case 2:
+      console.log("D");
       let D = new Audio("sounds/D.mp3");
       D.play();
       setTimeout("window.alert('D!')", 5000);
       break;
     case 3:
+      console.log("E");
       let E = new Audio("sounds/E.mp3");
       setTimeout("window.alert('E!')", 5000);
       E.play();
