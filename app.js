6a89c305b3d2c0285cc27c6eb674edcd77f09e7c
alittlebitofit <42311431+alittlebitofit@users.noreply.github.com>
2019-04-12 19:39:00 +0530

    added 3 minor chords
    Dm, Am, Em chords added, and a little bit of change in code, for better understanding

--- app.js
+++ app.js
@@ -1,40 +1,72 @@
 document.addEventListener("keydown", playChord);
+setTimeout("alert('Hello')");
 
 function playChord() {
-  let randomNumber = Math.floor(((Math.random() * 3))) + 1;
+  let randomNumber = Math.floor(((Math.random() * 6))) + 1;
 
   switch (randomNumber) {
     case 1:
-      console.log("A");
-      aChord();
-      setTimeout("alert('A!')", 5000);
+      console.log("D");
+      dMajorChord();
+      setTimeout("alert('D!')", 5000);
       break;
     case 2:
-      console.log("D");
-      dChord();
-      setTimeout("alert('D!')", 5000);
+      console.log("A");
+      aMajorChord();
+      setTimeout("alert('A!')", 5000);
       break;
     case 3:
       console.log("E");
-      eChord();
+      eMajorChord();
       setTimeout("alert('E!')", 5000);
+      break;
+    case 4:
+      console.log("D minor");
+      dMinorChord();
+      setTimeout("alert('D minor!')", 5000);
+      break;
+    case 5:
+      console.log("A minor");
+      aMinorChord();
+      setTimeout("alert('A minor!')", 5000);
+      break;
+    case 6:
+      console.log("E minor");
+      eMinorChord();
+      setTimeout("alert('E minor!')", 5000);
       break;
     default:
   }
 }
 
-function aChord() {
-  let A = new Audio("sounds/A.mp3");
+function aMinorChord() {
+  let a = new Audio("sounds/aminor.mp3");
+  a.play();
+}
+
+function dMinorChord() {
+  let d = new Audio("sounds/dminor.mp3");
+  d.play();
+}
+
+function eMinorChord() {
+  let e = new Audio("sounds/eminor.mp3");
+  e.play();
+}
+
+
+
+function aMajorChord() {
+  let A = new Audio("sounds/amajor.mp3");
   A.play();
 }
 
-function dChord() {
-  let D = new Audio("sounds/D.mp3");
+function dMajorChord() {
+  let D = new Audio("sounds/dmajor.mp3");
   D.play();
 }
 
-
-function eChord() {
-  let E = new Audio("sounds/E.mp3");
+function eMajorChord() {
+  let E = new Audio("sounds/emajor.mp3");
   E.play();
 }
